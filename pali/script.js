42e2820c0189880a83ddc8b558562351909de43e
fonzhousefonzie <harth.fonz@gmail.com>
2022-05-26 16:57:20 +0200

    pali

--- pali/script.js
+++ pali/script.js
@@ -2,16 +2,22 @@ const parolaInserita = prompt('Inserisci una parola palindroma');
 
     
     const parolaScomposta = parolaInserita.split("");
-    const parolaScompostaCapovolta = [];
-
-    console.log(parolaScomposta);
+    const parolaScompostaCapovolta = [];;
 
     for (let i = (parolaScomposta.length - 1) ; i >= 0; i--){
         parolaScompostaCapovolta.push(parolaScomposta[i]);
     }
     
-    if (parolaScomposta === parolaScompostaCapovolta){
-        console.log(true);;
-    } else {
-        console.log(false);;
+    let prova;
+
+    for (let i = 0; i < parolaScomposta.length; i++){
+        if (parolaScomposta[i] === parolaScompostaCapovolta[i]){
+            prova = true;
+        } else {
+            prova = false;
+        }
     }
+
+    console.log(parolaScomposta)
+    console.log(parolaScompostaCapovolta);
+    console.log(prova);
