de9ad8034c8ca33c37df4b94ca4ae770f7c8a680
fonzhousefonzie <harth.fonz@gmail.com>
2022-05-26 17:14:07 +0200

    pali

--- pali/script.js
+++ pali/script.js
@@ -11,6 +11,7 @@ const parolaInserita = prompt('Inserisci una parola palindroma');
     let prova;
 
     for (let i = 0; i < parolaScomposta.length; i++){
+        console.log("primo array: " + parolaScomposta[i] + " - secondo array: " + parolaScompostaCapovolta[i]);
         if (parolaScomposta[i] === parolaScompostaCapovolta[i]){
             prova = true;
         } else {
